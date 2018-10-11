22475796b132d6c1932512f2638c70cdc6407fbd
Antonio Chen <kk-c-bo@163.com>
2018-10-11 15:04:55 +0800

    test
    test1

---
+++ Swift_Rac_test1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift_Rac_test1
+//
+//  Created by 陈彦伯 on 2018/9/6.
+//  Copyright © 2018年 陈彦伯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("1")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
